5e0e851481fa73ca898f0a55adf596b0ee67f98c
Claudio Bizzarri <claudio@bizzarri.net>
2025-08-20 10:24:05 +0200

    Aggiunti alcuni CUSTOM SYSEX

--- custom_sysex_lookup.py
+++ custom_sysex_lookup.py
@@ -2,22 +2,43 @@
 # Lookup e struttura per Sysex personalizzati (non TABS/FOOTSWITCH standard)
 
 CUSTOM_SYSEX_LOOKUP = {
-    # Esempio: MICRO 1 ECHO SWITCH ON/OFF
+    "MICRO 1 SWITCH": {
+        "format": [0x26, 0x7B, 0x1D, 0x05, "switch"],
+        "switch_map": {
+            "toggle": 0x7F,
+            "off": 0x00,
+            "on": 0x01
+        }
+    },
+    "MICRO 1 TALK SWITCH": {
+        "format": [0x26, 0x7B, 0x21, 0x05, "switch"],
+        "switch_map": {
+            "toggle": 0x7F,
+            "off": 0x00,
+            "on": 0x01
+        }
+    },
     "MICRO 1 ECHO SWITCH": {
-        # La sequenza "format" include la parola chiave "switch" che verrà sostituita con il valore giusto
         "format": [0x26, 0x7B, 0x25, 0x05, "switch"],
         "switch_map": {
             "toggle": 0x7F,  # tipico per on/off toggle
             "off": 0x00,
             "on": 0x01
         }
-    }
-    # Puoi aggiungere qui altri custom, ad esempio:
-    # "MY CUSTOM ACTION": {
-    #     "format": [0x26, 0x7B, 0x12, "val1", "val2"],
-    #     "param_map": {
-    #         "up": 0x01,
-    #         "down": 0x02
-    #     }
-    # }
+    },
+    "VOICETRON ON/OFF": {
+        "format": [0x26, 0x7B, 0x36, 0x00, "switch"],
+        "switch_map": {
+            "off": 0x7f,
+            "on": 0x00
+        }
+    },
+    "VOICETRON SWITCH": {
+        "format": [0x26, 0x7B, 0x1E, 0x05, "switch"],
+        "switch_map": {
+            "toggle": 0x7F,  # tipico per on/off toggle
+            "off": 0x00,
+            "on": 0x01
+        }
+    },
 }
